a3be8b3686487d33fab7e6548ca206e1fd9ff2e2
Hari Jampana <hari.jampana@kony.com>
2016-10-18 15:46:17 +0530

    Basic test case using mocha

---
+++ test/test1.js
@@ -0,0 +1,14 @@
+var assert = chai.assert;
+describe('Main', function() {
+  describe('Test1', function() {
+    it('Test1 passed', function() {
+      assert.equal(-1,0);
+    });
+  });
+
+  describe('Test2', function() {
+    it('Test2 Passed', function() {
+      assert.equal(1,1);
+    });
+  });
+});
